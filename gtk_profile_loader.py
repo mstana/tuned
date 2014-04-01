ec697d5f4d935181fcd992acbf0081c83ce454e8
arashidak <rstanamarek@gmail.com>
2014-04-01 20:11:21 +0200

    format text and delete some useless control prints

--- gtk_profile_loader.py
+++ gtk_profile_loader.py
@@ -31,7 +31,6 @@ class Gtk_profile_loader(object):
         for i in profile.units.keys():
             print i
         
-        
 
     def test_print_all_loaded(self):
         print "test begin"  
@@ -39,14 +38,11 @@ class Gtk_profile_loader(object):
             print profile + " " + str(config.units.keys())
         print "test done"
         
-        
                 
     def load_profile_config(self, profile_name, path):
         conf_path = path + "/" + profile_name + "/tuned.conf"                
         profile_config = ConfigObj(conf_path)
         return profile_config
-
-
 #         FLAT VERSION
 #  
 #         conf_path = path + "/" + profile_name + "/tuned.conf"        
@@ -61,6 +57,7 @@ class Gtk_profile_loader(object):
 #                 return config
 #             
 #         return profile_config
+    
     
     def _locate_profile_path(self, profile_name):
         
@@ -84,18 +81,10 @@ class Gtk_profile_loader(object):
         config = ConfigObj()
         config.filename = path + tuned.consts.CONF_PROFILE_FILE
         config.initial_comment = "#", "tuned configuration","#"
-
-        print profile.units.items()    
-        print profile.name
-        print profile.options
-#         print profile.units["main"].options
         
         try:
             config["main"] = profile.options
-             
-#             {"include" : profile.options["include"]}
-        except KeyError: 
-            
+        except KeyError:
             pass #no problem just profile dont have include
 
         for name, unit in profile.units.items():
@@ -110,20 +99,20 @@ class Gtk_profile_loader(object):
     def get_names(self):
         return self.profiles.keys()
     
+    
     def get_profile(self, profile):
         return self.profiles[profile]
 
- 
-if __name__ == '__main__':
-      
-    if os.geteuid() != 0:
-        os.error("Superuser permissions are required to run the daemon.")
-        sys.exit(1)
-
-      
-    t = Gtk_profile_loader(tuned.consts.LOAD_DIRECTORIES)
-    
-#     pr = t.get_profile("sap")
-    
-    
-    t.save_profile(t.get_profile("sap"))
+# if __name__ == '__main__':
+#       
+#     if os.geteuid() != 0:
+#         os.error("Superuser permissions are required to run the daemon.")
+#         sys.exit(1)
+# 
+#       
+#     t = Gtk_profile_loader(tuned.consts.LOAD_DIRECTORIES)
+#     
+# #     pr = t.get_profile("sap")
+#     
+#     
+#     t.save_profile(t.get_profile("sap"))
